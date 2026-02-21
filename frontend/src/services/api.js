12dba16921efefee2b072561d0472cb576ac6ebd
Devavardhano6 <gatladevavardhan@gmail.com>
2026-02-21 11:57:01 +0530

    Fixed production API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,28 +1,21 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://campusflow-production.up.railway.app/api',
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
   },
 })
 
-// Request interceptor to add token
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token')
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
-  },
-  (error) => {
-    return Promise.reject(error)
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
   }
-)
+  return config
+})
 
-// Response interceptor to handle errors
 api.interceptors.response.use(
   (response) => response,
   (error) => {
